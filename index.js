fbeeeb7994a56d1f386db07db5bff44646829810
Srijan Manandhar <srijan.manandhar@gmail.com>
2025-02-14 15:22:10 +0545

    Add Prettier and ESLint configuration files for code formatting and linting

---
+++ index.js
@@ -0,0 +1,21 @@
+const configs = {
+  experimentalTernaries: true,
+  experimentalOperatorPosition: "start",
+  printWidth: 80, // default
+  tabWidth: 2, // default
+  useTabs: false, // default
+  semi: true, // default
+  singleQuote: false, // default
+  quoteProps: "as-needed", // default
+  trailingComma: "es5",
+  bracketSpacing: true, // default
+  objectWrap: "collapse",
+  bracketSameLine: false, // default
+  arrowParens: "always", // default
+  htmlWhitespaceSensitivity: "css", // default
+  endOfLine: "lf", // default
+  embeddedLanguageFormatting: "auto", // default
+  singleAttributePerLine: true,
+};
+
+export default configs;
